3ca4d00edcc89f1664392a7b635dd78dba9cf345
THINK2TRY <hzyjuly11@163.com>
2019-08-31 21:01:36 +0800

    add share interfece and adjust text layout

--- src/main/java/com/zhenyu/zhenyu/SingleNews.java
+++ src/main/java/com/zhenyu/zhenyu/SingleNews.java
@@ -4,8 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.nostra13.universalimageloader.utils.L;
 import com.zhenyu.zhenyu.Database.AppDatabase;
 import com.zhenyu.zhenyu.Database.NewsEntity;
 import com.zhenyu.zhenyu.user.UserProfile;
@@ -15,15 +17,22 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class SingleNews extends AppCompatActivity implements View.OnClickListener{
 
     private String newsid;
     private DataRepository dataRepository;
-    private FrameLayout bottomsheetlayout;
-    private BottomSheetBehavior bottomSheetBehavior;
+    private LinearLayout bottomsheetlayout;
+    private BottomSheetDialog bottomSheetBehavior;
+    private LinearLayout sharelayout;
+    private LinearLayout favoratelayout;
+    private LinearLayout blockinglayout;
 //    private TextView titleview;
 //    private TextView contentview;
 
@@ -45,8 +54,8 @@ public class SingleNews extends AppCompatActivity implements View.OnClickListene
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
-
-                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+                bottomSheetBehavior.show();
+//                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
             }
         });
 
@@ -69,9 +78,19 @@ public class SingleNews extends AppCompatActivity implements View.OnClickListene
         TextView timeView = findViewById(R.id.timeview);
         TextView sourceView = findViewById(R.id.sourceview);
 
+        String rawcontent = entity.getContent();
+        Pattern pattern = Pattern.compile("[\\n\\s]+");
+        Matcher matcher = pattern.matcher(rawcontent);
+        String replacement = "\n        ";
+        StringBuffer strn = new StringBuffer();
+        strn.append("  ");
+        while (matcher.find()){
+            matcher.appendReplacement(strn, replacement);
+        }
+        matcher.appendTail(strn);
 
         titleview.setText(entity.getTitle());
-        contentview.setText(entity.getContent());
+        contentview.setText(strn.toString());
         timeView.setText(entity.getPublishTime());
         sourceView.setText(entity.getPublisher());
 
@@ -81,16 +100,39 @@ public class SingleNews extends AppCompatActivity implements View.OnClickListene
     }
 
     public void initbottonsheet(){
-        bottomsheetlayout = (FrameLayout)findViewById(R.id.bottomsheet);
-        bottomSheetBehavior = BottomSheetBehavior.from(bottomsheetlayout);
-        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
+
+        bottomsheetlayout = (LinearLayout) findViewById(R.id.bottomsheet);
+        bottomSheetBehavior = new BottomSheetDialog(this);
+        View view = getLayoutInflater().inflate(R.layout.dialog_bottom, null);
+        bottomSheetBehavior.setContentView(view);
+
+        sharelayout = view.findViewById(R.id.share_item);
+        favoratelayout = view.findViewById(R.id.favorate_item);
+        blockinglayout = view.findViewById(R.id.blocking_item);
+        sharelayout.setOnClickListener(this);
+        favoratelayout.setOnClickListener(this);
+        blockinglayout.setOnClickListener(this);
+
+//        bottomSheetBehavior = BottomSheetBehavior.from(bottomsheetlayout);
+//        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()){
-
+            case R.id.share_item:
+                Toast.makeText(getApplicationContext(), "click share", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.favorate_item:
+                Toast.makeText(getApplicationContext(), "click favorate", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.blocking_item:
+                Toast.makeText(getApplicationContext(), "click blocking", Toast.LENGTH_LONG).show();
+                break;
+            default:
+                    break;
         }
+
     }
 
 }
